7dbed1bce813ee7f531d455353e92efcbfbd0488
Roshan Alwis <roshanalwis@Roshans-MacBook-Pro.local>
2020-08-03 22:52:55 +0530

    Updating changes

---
+++ build/precache-manifest.764bcf83d45fabf8c80b17db1fc68f20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2b933393a7991ed737bf20dda06a15",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e027ddd17f4c96aee01",
+    "url": "/monsters-rolodex/static/css/main.4f6fd437.chunk.css"
+  },
+  {
+    "revision": "69963911d770d3a1f832",
+    "url": "/monsters-rolodex/static/js/2.bb882d51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.bb882d51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e027ddd17f4c96aee01",
+    "url": "/monsters-rolodex/static/js/main.e5d1304c.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
